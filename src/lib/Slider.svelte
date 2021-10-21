70a1d2f4b1834d32d50533df21554eae2900759b
moritzebeling <git@moritzebeling.com>
2021-10-21 19:19:39 +0200

    slider

---
+++ src/lib/Slider.svelte
@@ -0,0 +1,30 @@
+<script>
+
+    export let name = "Slider";
+    export let min = 0;
+    export let max = 100;
+    export let value = 50;
+
+    let id = `slider-${name.toLowerCase()}`;
+
+</script>
+
+<div class="field">
+    <label for="{id}">{name}</label>
+    <input type="range" {min} {max} {value}>
+</div>
+
+<style lang="scss">
+
+    .field {
+        margin: 1rem;
+        display: flex;
+    }
+    label {
+        flex: 1 0 30%;
+    }
+    input {
+        flex: 1 0 70%;
+    }
+
+</style>
---
+++ src/routes/interface/_Controls.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+</script>
+
+<script>
+
+    import Slider from '$lib/Slider.svelte';
+
+</script>
+
+<div class="controls">
+
+    <Slider name="Slider" />
+    <Slider name="Era" />
+    <Slider name="Complexity" />
+    <Slider name="Absurdity" />
+    <Slider name="Materiality" />
+    <Slider name="Q-Structure" />
+    <Slider name="Level" />
+    <Slider name="Order" />
+    <Slider name="Style-semantic ralationship" />
+    <Slider name="Contemporary Appriximator" />
+    <Slider name="Noise injection" />
+    <Slider name="Aether path deriviation" />
+
+</div>
+
+<style lang="scss">
+</style>
