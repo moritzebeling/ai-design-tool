37a43512abbfba4f68ea14c4dd4b3fe59ab5255f
moritzebeling <git@moritzebeling.com>
2021-10-22 10:21:36 +0200

    reinstall sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,9 @@
-import adapter from '@sveltejs/adapter-netlify';
-import sveltePreprocess from 'svelte-preprocess';
-
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sveltePreprocess({
-		defaults: {
-			style: 'scss'
-		},
-		scss: {
-			// relative to root
-			prependData: `@import 'src/scss/_mixins.scss';`
-		}
-	}),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter(),
+		target: '#svelte'
 	}
 };
 
 export default config;
-
-
